51eb87170418ce2a1a43ecce43d697d35e3dca34
lucas.wang <lucas.wang@ariix.com>
2017-12-13 15:41:38 +0800

    添加项目文件。

---
+++ BingWallpaper/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 必应壁纸
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool flag = false;
+                System.Threading.Mutex mutex = new System.Threading.Mutex(true, "bingwallpaper", out flag);
+                if (flag)
+                {
+                    MyFiles.Init();
+                    Form_Main fm = new Form_Main();
+                    if (args.Length > 0)
+                    {
+                        if (args[0] == @"/onekey")
+                        {
+                            OneKey = true;
+                            fm.Opacity = 0;
+                            fm.ShowInTaskbar = false;
+                            fm.Size = new System.Drawing.Size(1, 1);
+                            fm.Opacity = 0;
+                            fm.ShowDialog();
+                        }
+                        else
+                        {
+                            OneKey = false;
+                            fm.ShowDialog();
+                        }
+                    }
+                    else
+                    {
+                        fm.ShowDialog();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public static bool OneKey;
+    }
+}
